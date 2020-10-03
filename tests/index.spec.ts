fa8aa5e771b0577c9e47a43d1796c329ac72336c
Pepe Becker <mail@pepebecker.com>
2020-10-03 14:04:18 +0900

    Update project to TypeScript project

---
+++ tests/index.spec.ts
@@ -0,0 +1,75 @@
+'use strict'
+
+import * as utils from '../src/index'
+import { expect } from 'chai'
+import syllables from './syllables'
+
+describe('Codepoint to Unicode', () => {
+  it('should convert codepoint "U+6211" into 我', () => {
+    expect(utils.codepointToUnicode('U+6211')).equal('我')
+  })
+
+  it('should convert codepoint "0x6211" into 我', () => {
+    expect(utils.codepointToUnicode('0x6211')).equal('我')
+  })
+
+  it('should convert codepoint 0x6211 into 我', () => {
+    expect(utils.codepointToUnicode(0x6211)).equal('我')
+  })
+
+  it('should convert codepoint "6211" into 我', () => {
+    expect(utils.codepointToUnicode('6211')).equal('我')
+  })
+})
+
+describe('Tone number', () => {
+  it('should get the right tone number', () => {
+    for (const s of syllables) {
+      expect(utils.getToneNumber(s[0])).equal(5)
+      expect(utils.getToneNumber(s[1])).equal(1)
+      expect(utils.getToneNumber(s[2])).equal(2)
+      expect(utils.getToneNumber(s[3])).equal(3)
+      expect(utils.getToneNumber(s[4])).equal(4)
+    }
+  })
+})
+
+describe('Remove tone', () => {
+  it('should remove the tone correctly', () => {
+    for (const s of syllables) {
+      expect(utils.removeTone(s[0])).equal(s[0])
+      expect(utils.removeTone(s[1])).equal(s[0])
+      expect(utils.removeTone(s[2])).equal(s[0])
+      expect(utils.removeTone(s[3])).equal(s[0])
+      expect(utils.removeTone(s[4])).equal(s[0])
+
+      expect(utils.removeTone(s[0] + '1')).equal(s[0])
+      expect(utils.removeTone(s[0] + '2')).equal(s[0])
+      expect(utils.removeTone(s[0] + '3')).equal(s[0])
+      expect(utils.removeTone(s[0] + '4')).equal(s[0])
+      expect(utils.removeTone(s[0] + '5')).equal(s[0])
+    }
+  })
+})
+
+describe('Convert', () => {
+  it('should convert tone number to mark correctly', () => {
+    for (const s of syllables) {
+      expect(utils.numberToMark(s[0])).equal(s[0])
+      expect(utils.numberToMark(s[0] + '5')).equal(s[0])
+      expect(utils.numberToMark(s[0] + '1')).equal(s[1])
+      expect(utils.numberToMark(s[0] + '2')).equal(s[2])
+      expect(utils.numberToMark(s[0] + '3')).equal(s[3])
+      expect(utils.numberToMark(s[0] + '4')).equal(s[4])
+    }
+  })
+  it('should convert mark to tone number correctly', () => {
+    for (const s of syllables) {
+      expect(utils.markToNumber(s[0])).equal(s[0] + '5')
+      expect(utils.markToNumber(s[1])).equal(s[0] + '1')
+      expect(utils.markToNumber(s[2])).equal(s[0] + '2')
+      expect(utils.markToNumber(s[3])).equal(s[0] + '3')
+      expect(utils.markToNumber(s[4])).equal(s[0] + '4')
+    }
+  })
+})
